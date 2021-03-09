f5596543bcc143fe107189bd91581dc22460c7ae
Fer Wicker <fer.wicker@gmail.com>
2021-03-09 20:04:58 +1100

    travis test again

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,9 +18,9 @@ id: req.user.id
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", (req, res) => {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password
+  db.User.create({
+  email: req.body.email,
+  password: req.body.password
     })
       .then(() => {
         res.redirect(307, "/api/login");
