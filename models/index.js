3dfaa4f24f635d656f0e52fe5f8afacde6d1bcda
Thu Van <luuluu_1975@yahoo.com.au>
2021-03-11 18:46:14 +1100

    Updated connection and table create

--- models/index.js
+++ models/index.js
@@ -1,36 +1,59 @@
-'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+// Sequelize (capital) references the standard library
+const Sequelize = require('sequelize');
+// sequelize (lowercase) references my connection to the DB.
+const sequelize = require('../config/config.js');
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(function(modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+// Creates a "Book" model that matches up with DB
+const Messages = sequelize.define('messages', {
+  message_desc: Sequelize.STRING,
+  message_cat_id: Sequelize.STRING,
+  likes: Sequelize.INTEGER,
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+const Message_Cat = sequelize.define('message_cat', {
+  cat_desc: Sequelize.STRING,
+});
 
-module.exports = db;
+// Syncs with DB
+message_wall.sync();
+
+// Makes the Book Model available for other files (will also create a table)
+module.exports = message_wall;
+
+// 'use strict';
+
+// var fs        = require('fs');
+// var path      = require('path');
+// var Sequelize = require('sequelize');
+// var basename  = path.basename(module.filename);
+// var env       = process.env.NODE_ENV || 'development';
+// var config    = require(__dirname + '/../config/config.json')[env];
+// var db        = {};
+
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } else {
+//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+// fs
+//   .readdirSync(__dirname)
+//   .filter(function(file) {
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(function(file) {
+//     var model = sequelize['import'](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(function(modelName) {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
