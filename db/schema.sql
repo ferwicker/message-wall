7a24521e145e15e82cd5f76ae6fd06cd9fae3369
Fer Wicker <fer.wicker@gmail.com>
2021-03-20 13:18:03 +1100

    updated db files and package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,32 @@
---  * Create schema.
-DROP DATABASE IF EXISTS message_wall;
-CREATE DATABASE message_wall;
+DROP DATABASE IF EXISTS messagewall_db;
+CREATE DATABASE messagewall_db;
 
--- * create tables.
+USE messagewall_db;
 
-USE message_wall;
-
-CREATE TABLE messages (
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-message_desc VARCHAR(100),
-message_cat_id INT,
-likes INT,
-font_id INT,
-colour_id INT,
-FOREIGN KEY (font_id) REFERENCES fonts(id),
-FOREIGN KEY (colour_id) REFERENCES colour(id)
-);
-
-CREATE TABLE message_cat (
+CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    cat_desc VARCHAR(100)
+    category_label VARCHAR(100)
 );
 
 CREATE TABLE fonts (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    font_label VARCHAR(50),
-    css_col VARCHAR(50)
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    font_label VARCHAR(100),
+    font_css TEXT
 );
 
-CREATE TABLE colour (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    col_label VARCHAR(50),
-    css_name VARCHAR(50)
+CREATE TABLE colours (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    colour_label VARCHAR(100),
+    colour_css TEXT
+);
+
+CREATE TABLE messages (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    message_body VARCHAR(140),
+	category_id INT,
+    font_id INT, 
+    colour_id INT,
+    FOREIGN KEY (category_id) REFERENCES fonts(id),
+    FOREIGN KEY (font_id) REFERENCES fonts(id),
+	FOREIGN KEY (colour_id) REFERENCES colours(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,32 @@
-INSERT into message_cat (cat_desc) VALUES
-("Things to do before I die");
+INSERT INTO categories (category_label) VALUES 
+	('Before I die I want to...'), 
+	("I'm thankful for..."), 
+	('My crazy dreams...');
 
-INSERT into fonts (font_label, css_col) VALUES
-("Montserrat Black" , "fonts"),
-("New Spirit Medium" , "fonts"),
-("IvyMode Italic", "fonts");
+INSERT INTO fonts (font_label, font_css) VALUES 
+	('montserrat', 
+    'font-family: montserrat, sans-serif;
+    font-weight: 800;
+    font-size: 40px;
+    font-style: normal;'),
+    ('newspirit', 
+    'font-family: new-spirit, serif;
+    font-weight: 500;
+    font-size: 40px;
+    font-style: normal;'),
+    ('ivymode', 
+    'font-family: ivymode,sans-serif;
+    font-weight: 400;
+    font-size: 40px;
+    font-style: italic;');
 
-INSERT INTO colour (col_label, css_name) VALUES
-("Aqua", "colours"),
-("Tan", "colours"),
-("Forest Green", "colours"),
-("Cream", "colours");
+INSERT INTO colours (colour_label, colour_css) VALUES
+	('aqua', 'color: #6dc1b1;'),
+    ('tan', 'color: #c4b887;'),
+    ('forestgreen', 'color: #074338;'),
+    ('cream', 'color: #f7f5ed;');
+
+INSERT INTO messages (message_body, category_id, font_id, colour_id) VALUES
+('Leave a legacy', '1', '2', '4'),
+('Whoever invented chocolate', '2', '2', '3'),
+('Riding a unicorn on the clouds', '3', '3', '1');
