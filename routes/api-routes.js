4e08a955e022482a4761d27e1e1f73639ba0bea0
Fer Wicker <fer.wicker@gmail.com>
2021-03-22 09:28:23 +1100

    old stuff cleanup

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,31 +3,6 @@ const { promises } = require('fs');
 const db = require("../models");
 
 module.exports = function(app) {
-  //get messages
-  /* // does not render
-  app.get("/api/wall/:category_id", (req, res) => {
-    const wallId = req.params.category_id;
-    console.log(req.params);
-    console.log(`api id value: ${wallId}`);
-    const messages = db.messages.findAll({
-      raw : true,
-      where: {
-        category_id: wallId
-      }});
-    const categories = db.categories.findAll({raw : true});
-    const fonts = db.fonts.findAll({raw : true});
-    const colours = db.colours.findAll({raw : true});
-    Promise.all([categories, fonts, colours, messages]).then((data) => {
-      const renderObj = {
-        categories: data[0],
-        fonts: data[1],
-        colours: data[2],
-        messages: data[3]
-      };
-      console.log(renderObj);
-      res.render('index', renderObj);
-    }).catch(err => console.log(err));
-  }); */
 
   //create new message
   app.post("/api/messages", (req, res) => {
@@ -41,18 +16,4 @@ module.exports = function(app) {
     });
   });
 
-  //   // PUT route for updating todos. We can get the updated todo data from req.body
-  //   app.put('/api/todos', (req, res) => {
-  //     db.Todo.update(
-  //       {
-  //         text: req.body.text,
-  //         complete: req.body.complete,
-  //       },
-  //       {
-  //         where: {
-  //           id: req.body.id,
-  //         },
-  //       }
-  //     ).then((dbTodo) => res.json(dbTodo));
-  //   });
 };
