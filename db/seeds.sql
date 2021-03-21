6ff26a3842f55d7cabb220af7a19319b21234d09
Fer Wicker <fer.wicker@gmail.com>
2021-03-21 19:11:22 +1100

    Media queries for mobile responsiveness

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,18 +4,18 @@ INSERT INTO categories (category_label) VALUES
 	('My crazy dreams...');
 
 INSERT INTO fonts (font_label, font_css) VALUES 
-	('montserrat', 
-    'font-family: montserrat, sans-serif; font-weight: 800; font-size: 40px; font-style: normal;'),
-    ('newspirit', 
-    'font-family: new-spirit, serif; font-weight: 500; font-size: 40px; font-style: normal;'),
-    ('ivymode', 
-    'font-family: ivymode,sans-serif; font-weight: 400; font-size: 40px; font-style: italic;');
+	('Montserrat', 
+    'font-family: montserrat, sans-serif; font-weight: 800; font-style: normal;'),
+    ('New Spirit', 
+    'font-family: new-spirit, serif; font-weight: 500; font-style: normal;'),
+    ('IvyMode', 
+    'font-family: ivymode,sans-serif; font-weight: 400; font-style: italic;');
 
 INSERT INTO colours (colour_label, colour_css) VALUES
-	('aqua', 'color: #6dc1b1;'),
-    ('tan', 'color: #c4b887;'),
-    ('forestgreen', 'color: #074338;'),
-    ('cream', 'color: #f7f5ed;');
+	('Aqua', 'color: #6dc1b1;'),
+    ('Tan', 'color: #c4b887;'),
+    ('Forest Green', 'color: #074338;'),
+    ('Cream', 'color: #f7f5ed;');
 
 INSERT INTO messages (message_body, category_id, font_id, colour_id) VALUES
 ('Leave a legacy', '1', '2', '4'),
